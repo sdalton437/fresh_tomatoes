fcc92353aee3f5c2e4182d273e192556cb6d26c0
sdalton437 <daltonsam437@gmail.com>
2017-06-14 21:03:52 -0400

    fixed formatting issues

--- media.py
+++ media.py
@@ -1,17 +1,17 @@
-#Import necessary files
+# Import necessary files
 import webbrowser
 
+
 class Movie():
-    """This class stores movie information for use in fresh_tomatoes.py"""      
-    
+    """This class stores movie information for use in fresh_tomatoes.py"""
+
     def __init__(self, movie_title, movie_storyline, poster_image,
-               trailer_youtube):
-        self.title = movie_title  #stores movie title
-        self.storyline = movie_storyline  #stores movie storyline
-        self.poster_image_url = poster_image  #stores poster image url
-        self.trailer_youtube_url = trailer_youtube  #stores youtube trailer url
+                 trailer_youtube):
+        self.title = movie_title  # stores movie title
+        self.storyline = movie_storyline  # stores movie storyline
+        self.poster_image_url = poster_image  # stores poster image url
+        self.trailer_youtube_url = trailer_youtube  # stores youtube trailer url # noqa
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)  #Used to open movie's youtube
-                                                   #trailer
-        
+        # Used to open movie's youtube trailer
+        webbrowser.open(self.trailer_youtube_url)
